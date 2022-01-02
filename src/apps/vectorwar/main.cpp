86300c4a9ec4004edf76e7316b8ee262645131fe
Tom Solacroup <tom.solacroup@gmail.com>
2022-01-02 14:22:47 +0100

    wtf is a bucket id???

--- src/apps/vectorwar/main.cpp
+++ src/apps/vectorwar/main.cpp
@@ -10,10 +10,16 @@
 #include "eos_connect.h"
 #include "eos_init.h"
 #include "eos_sdk.h"
+#include "eos_lobby.h"
+#include "eos_logging.h"
+#include <string>
 #include "../../../eos_secrets.h"
 
 EOS_HPlatform g_EOS_Platform_Handle;
 EOS_HConnect g_EOS_Connect_Handle;
+EOS_HLobby g_EOS_Lobby_Handle;
+EOS_ProductUserId g_EOS_LocalUserId;
+bool g_bEOS_Connect_Succeeded = false;
 
 LRESULT CALLBACK
 MainWindowProc(HWND hwnd,
@@ -72,6 +78,26 @@ CreateMainWindow(HINSTANCE hInstance)
 }
 
 void
+_Execute_EOS_Lobby_CreateLobby()
+{
+    g_EOS_Lobby_Handle = EOS_Platform_GetLobbyInterface(g_EOS_Platform_Handle);
+
+    EOS_Lobby_CreateLobbyOptions createLobbyOptions = {};
+    createLobbyOptions.ApiVersion = EOS_LOBBY_CREATELOBBY_API_LATEST;
+    createLobbyOptions.PermissionLevel = EOS_ELobbyPermissionLevel::EOS_LPL_PUBLICADVERTISED;
+    createLobbyOptions.LocalUserId = g_EOS_LocalUserId;
+    createLobbyOptions.MaxLobbyMembers = 2;
+    createLobbyOptions.bAllowInvites = true;
+    createLobbyOptions.bDisableHostMigration = false;
+    createLobbyOptions.bEnableRTCRoom = false;
+    createLobbyOptions.LocalRTCOptions = NULL;
+    createLobbyOptions.BucketId = "wtf";
+    EOS_Lobby_CreateLobby(g_EOS_Lobby_Handle, &createLobbyOptions, NULL, [](const EOS_Lobby_CreateLobbyCallbackInfo* info) {
+        printf("%d", info->ResultCode);
+    });
+}
+
+void
 RunMainLoop(HWND hwnd)
 {
    MSG msg = { 0 };
@@ -80,6 +106,10 @@ RunMainLoop(HWND hwnd)
    start = next = now = timeGetTime();
    while(1) {
       EOS_Platform_Tick(g_EOS_Platform_Handle);
+      if (g_bEOS_Connect_Succeeded) {
+          _Execute_EOS_Lobby_CreateLobby();
+      }
+
       while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
          TranslateMessage(&msg); 
          DispatchMessage(&msg);
@@ -104,6 +134,10 @@ Syntax()
               L"Could not start", MB_OK);
 }
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <iostream>
+#include <sstream>
 void
 _Execute_EOS_Init()
 {
@@ -124,6 +158,12 @@ _Execute_EOS_Init()
     platformOptions.bIsServer = EOS_FALSE;
     g_EOS_Platform_Handle = EOS_Platform_Create(&platformOptions);
     g_EOS_Connect_Handle = EOS_Platform_GetConnectInterface(g_EOS_Platform_Handle);
+
+    EOS_Logging_SetCallback([](const EOS_LogMessage* info) {
+        std::wostringstream os_;
+        os_ << info->Message << std::endl;
+        OutputDebugString(os_.str().c_str());
+    });
 }
 
 void
@@ -148,12 +188,14 @@ _Execute_EOS_Login()
             MessageBox(NULL,
                 L"EOS_Connect_Login succeeded!\n",
                 L"Success!", MB_OK);
+            g_bEOS_Connect_Succeeded = true;
+            g_EOS_LocalUserId = info->LocalUserId;
         }
     });
 }
 
 void
-_Execute_EOS_Connect_CreateDeviceId()
+_Execute_EOS_Connect_CreateDeviceId_Then_Login()
 {
     EOS_Connect_CreateDeviceIdOptions createDeviceIdOptions = {};
     createDeviceIdOptions.ApiVersion = EOS_CONNECT_CREATEDEVICEID_API_LATEST;
@@ -172,10 +214,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_ int)
 {
    HWND hwnd = CreateMainWindow(hInstance);
-   int offset = 1, local_player = 0;
+   //int offset = 1, local_player = 0;
    WSADATA wd = { 0 };
-   wchar_t wide_ip_buffer[128];
-   unsigned int wide_ip_buffer_size = (unsigned int)ARRAYSIZE(wide_ip_buffer);
+   //wchar_t wide_ip_buffer[128];
+   //unsigned int wide_ip_buffer_size = (unsigned int)ARRAYSIZE(wide_ip_buffer);
 
    WSAStartup(MAKEWORD(2, 2), &wd);
    POINT window_offsets[] = {
@@ -189,68 +231,72 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
    _Execute_EOS_Init();
-   _Execute_EOS_Connect_CreateDeviceId();
+   _Execute_EOS_Connect_CreateDeviceId_Then_Login();
 
-   if (__argc < 3) {
-      Syntax();
-      return 1;
-   }
-   unsigned short local_port = (unsigned short)_wtoi(__wargv[offset++]);
-   int num_players = _wtoi(__wargv[offset++]);
-   if (num_players < 0 || __argc < offset + num_players) {
-      Syntax();
-      return 1;
-   }
-   if (wcscmp(__wargv[offset], L"spectate") == 0) {
-      char host_ip[128];
-      unsigned short host_port;
-      if (swscanf_s(__wargv[offset+1], L"%[^:]:%hu", wide_ip_buffer, wide_ip_buffer_size, &host_port) != 2) {
-         Syntax();
-         return 1;
-      }
-      wcstombs_s(nullptr, host_ip, ARRAYSIZE(host_ip), wide_ip_buffer, _TRUNCATE);
-      VectorWar_InitSpectator(hwnd, local_port, num_players, host_ip, host_port);
-   } else {
-      GGPOPlayer players[GGPO_MAX_SPECTATORS + GGPO_MAX_PLAYERS];
+   //{
+   //    if (__argc < 3) {
+   //        Syntax();
+   //        return 1;
+   //    }
+   //    unsigned short local_port = (unsigned short)_wtoi(__wargv[offset++]);
+   //    int num_players = _wtoi(__wargv[offset++]);
+   //    if (num_players < 0 || __argc < offset + num_players) {
+   //        Syntax();
+   //        return 1;
+   //    }
+   //    if (wcscmp(__wargv[offset], L"spectate") == 0) {
+   //        char host_ip[128];
+   //        unsigned short host_port;
+   //        if (swscanf_s(__wargv[offset + 1], L"%[^:]:%hu", wide_ip_buffer, wide_ip_buffer_size, &host_port) != 2) {
+   //            Syntax();
+   //            return 1;
+   //        }
+   //        wcstombs_s(nullptr, host_ip, ARRAYSIZE(host_ip), wide_ip_buffer, _TRUNCATE);
+   //        VectorWar_InitSpectator(hwnd, local_port, num_players, host_ip, host_port);
+   //    }
+   //    else {
+   //        GGPOPlayer players[GGPO_MAX_SPECTATORS + GGPO_MAX_PLAYERS];
 
-      int i;
-      for (i = 0; i < num_players; i++) {
-         const wchar_t *arg = __wargv[offset++];
+   //        int i;
+   //        for (i = 0; i < num_players; i++) {
+   //            const wchar_t* arg = __wargv[offset++];
 
-         players[i].size = sizeof(players[i]);
-         players[i].player_num = i + 1;
-         if (!_wcsicmp(arg, L"local")) {
-            players[i].type = GGPO_PLAYERTYPE_LOCAL;
-            local_player = i;
-            continue;
-         }
-         
-         players[i].type = GGPO_PLAYERTYPE_REMOTE;
-         if (swscanf_s(arg, L"%[^:]:%hd", wide_ip_buffer, wide_ip_buffer_size, &players[i].u.remote.port) != 2) {
-            Syntax();
-            return 1;
-         }
-         wcstombs_s(nullptr, players[i].u.remote.ip_address, ARRAYSIZE(players[i].u.remote.ip_address), wide_ip_buffer, _TRUNCATE);
-      }
-      // these are spectators...
-      int num_spectators = 0;
-      while (offset < __argc) {
-         players[i].type = GGPO_PLAYERTYPE_SPECTATOR;
-         if (swscanf_s(__wargv[offset++], L"%[^:]:%hd", wide_ip_buffer, wide_ip_buffer_size, &players[i].u.remote.port) != 2) {
-            Syntax();
-            return 1;
-         }
-         wcstombs_s(nullptr, players[i].u.remote.ip_address, ARRAYSIZE(players[i].u.remote.ip_address), wide_ip_buffer, _TRUNCATE);
-         i++;
-         num_spectators++;
-      }
+   //            players[i].size = sizeof(players[i]);
+   //            players[i].player_num = i + 1;
+   //            if (!_wcsicmp(arg, L"local")) {
+   //                players[i].type = GGPO_PLAYERTYPE_LOCAL;
+   //                local_player = i;
+   //                continue;
+   //            }
 
-      if (local_player < sizeof(window_offsets) / sizeof(window_offsets[0])) {
-         ::SetWindowPos(hwnd, NULL, window_offsets[local_player].x, window_offsets[local_player].y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-      }
+   //            players[i].type = GGPO_PLAYERTYPE_REMOTE;
+   //            if (swscanf_s(arg, L"%[^:]:%hd", wide_ip_buffer, wide_ip_buffer_size, &players[i].u.remote.port) != 2) {
+   //                Syntax();
+   //                return 1;
+   //            }
+   //            wcstombs_s(nullptr, players[i].u.remote.ip_address, ARRAYSIZE(players[i].u.remote.ip_address), wide_ip_buffer, _TRUNCATE);
+   //        }
+   //        // these are spectators...
+   //        int num_spectators = 0;
+   //        while (offset < __argc) {
+   //            players[i].type = GGPO_PLAYERTYPE_SPECTATOR;
+   //            if (swscanf_s(__wargv[offset++], L"%[^:]:%hd", wide_ip_buffer, wide_ip_buffer_size, &players[i].u.remote.port) != 2) {
+   //                Syntax();
+   //                return 1;
+   //            }
+   //            wcstombs_s(nullptr, players[i].u.remote.ip_address, ARRAYSIZE(players[i].u.remote.ip_address), wide_ip_buffer, _TRUNCATE);
+   //            i++;
+   //            num_spectators++;
+   //        }
 
-      VectorWar_Init(hwnd, local_port, num_players, players, num_spectators);
-   }
+   //        if (local_player < sizeof(window_offsets) / sizeof(window_offsets[0])) {
+   //            ::SetWindowPos(hwnd, NULL, window_offsets[local_player].x, window_offsets[local_player].y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+   //        }
+
+   //        VectorWar_Init(hwnd, local_port, num_players, players, num_spectators);
+   //    }
+   //}
+
    RunMainLoop(hwnd);
    VectorWar_Exit();
    WSACleanup();
